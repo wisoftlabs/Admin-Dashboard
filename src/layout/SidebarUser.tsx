f6c1d112629840bad2d2d2c8afeddf893c8ce7c4
R3pic <101osc@naver.com>
2025-12-02 16:29:12 +0900

    feat: sidebar user

--- src/layout/SidebarUser.tsx
+++ src/layout/SidebarUser.tsx
@@ -1,20 +1,31 @@
 import {Item, ItemActions, ItemContent, ItemDescription, ItemMedia, ItemTitle} from "@/components/ui/item";
-import {UserIcon} from "lucide-react";
+import WisoftLogo from "@/assets/wisoft-fav.webp";
 import {Button} from "@/components/ui/button";
+import {SidebarMenu, SidebarMenuItem} from "@/components/ui/sidebar";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
 
 export function SidebarUser() {
-  return <Item>
-    <ItemMedia>
-      <UserIcon className="size-8"/>
-    </ItemMedia>
-    <ItemContent>
-      <ItemTitle>관리자</ItemTitle>
-      <ItemDescription>환영합니다.</ItemDescription>
-    </ItemContent>
-    <ItemActions>
-      <Button>
-        연장
-      </Button>
-    </ItemActions>
-  </Item>
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <Item variant="outline">
+          <ItemMedia>
+            <Avatar>
+              <AvatarImage src={WisoftLogo}/>
+              <AvatarFallback>WS</AvatarFallback>
+            </Avatar>
+          </ItemMedia>
+          <ItemContent>
+            <ItemTitle>관리자</ItemTitle>
+            <ItemDescription>환영합니다.</ItemDescription>
+          </ItemContent>
+          <ItemActions>
+            <Button onClick={() => alert("연장 버튼 클릭!")}>
+              연장
+            </Button>
+          </ItemActions>
+        </Item>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  );
 }
