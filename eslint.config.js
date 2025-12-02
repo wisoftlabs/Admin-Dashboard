b0f5c52e3410b5c6f45123c9275e10a05764c058
R3pic <101osc@naver.com>
2025-12-02 16:06:54 +0900

    chore: eslint formatter stylistic

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,17 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
+    plugins: {
+      '@stylistic': stylistic
+    },
     files: ['**/*.{ts,tsx}'],
+    ignores: ['./src/components/ui/*.tsx'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -19,5 +24,9 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/quotes': ["error", "double"]
+    }
   },
 ])
